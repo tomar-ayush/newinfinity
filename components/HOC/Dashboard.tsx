f13292d994128e6bbb2c9c8958dd88f3a6f00bde
Aryainguz <aryaninguz369@gmail.com>
2024-12-09 23:20:15 +0530

    refactor: update Dashboard layout for improved responsiveness and styling

--- components/HOC/Dashboard.tsx
+++ components/HOC/Dashboard.tsx
@@ -80,11 +80,27 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
       </Sidebar>
 
       {/* Main Content */}
-      <div className="flex flex-1 flex-col">
-        <div className="p-10 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex-1">
+      <div className="flex flex-1">
+        <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+          {/* skeleton loader */}
+          {/* <div className="flex gap-2">
+          {[...new Array(4)].map((i) => (
+            <div
+              key={"first-array" + i}
+              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            ></div>
+          ))}
+        </div>
+        <div className="flex gap-2 flex-1">
+          {[...new Array(2)].map((i) => (
+            <div
+              key={"second-array" + i}
+              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            ></div>
+          ))}
+        </div> */}
           {children}
         </div>
-        {/* Dark/Light Mode Toggle */}
         <div className="relative top-2">
           <ModeToggle />
         </div>
